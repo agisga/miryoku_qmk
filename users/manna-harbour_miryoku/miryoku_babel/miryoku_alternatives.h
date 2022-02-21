681bd8920b12e0f18528a08843b8ed9b15989aaa
agisga <11449372+agisga@users.noreply.github.com>
2022-02-20 21:55:52 -0500

    removes SYM layer because it's exactly shifted NUM and should be accessed as such (NUM -> F key; and autoshift is also enabled on NUM)

--- users/manna-harbour_miryoku/miryoku_babel/miryoku_alternatives.h
+++ users/manna-harbour_miryoku/miryoku_babel/miryoku_alternatives.h
@@ -57,9 +57,9 @@ U_NP,              U_NP,              LT(FUN, KC_DEL),   LT(NUM, KC_BSPC),  LT(S
 
 #define MIRYOKU_ALTERNATIVES_BASE_QWERTY_FLIP \
 KC_Q,              KC_W,              KC_E,              KC_R,              KC_T,              KC_Y,              KC_U,              KC_I,              KC_O,              KC_P,              \
-KC_A,              KC_S,              KC_D,              LT(SYM, KC_F),     KC_G,              KC_H,              KC_J,              KC_K,              KC_L,              KC_QUOT,   \
+KC_A,              KC_S,              KC_D,              KC_F,              KC_G,              KC_H,              KC_J,              KC_K,              KC_L,              KC_QUOT,   \
 KC_Z,              KC_X,              KC_C,              KC_V,              KC_B,              KC_N,              KC_M,              KC_COMM,           KC_DOT,            KC_SLSH, \
-U_NP,              U_NP,              LT(FUN, KC_DEL),   LT(NUM, KC_BSPC),  OSM(MOD_LSFT),   LT(MOUSE, KC_TAB), LT(NAV, KC_SPC),   LT(MEDIA, KC_ESC), U_NP,              U_NP
+U_NP,              U_NP,              LT(FUN, KC_DEL),   LT(NUM, KC_BSPC),  OSM(MOD_LSFT),     LT(MOUSE, KC_TAB), LT(NAV, KC_SPC),   LT(MEDIA, KC_ESC), U_NP,              U_NP
 
 #define MIRYOKU_ALTERNATIVES_BASE_QWERTZ_FLIP \
 KC_Q,              KC_W,              KC_E,              KC_R,              KC_T,              KC_Z,              KC_U,              KC_I,              KC_O,              KC_P,              \
