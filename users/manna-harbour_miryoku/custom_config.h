b42fa5f5c9c67aff5cdf94c7da5a34a9de53dab5
agisga <11449372+agisga@users.noreply.github.com>
2022-06-25 15:44:31 -0400

    new locations for BSPC and TAB

--- users/manna-harbour_miryoku/custom_config.h
+++ users/manna-harbour_miryoku/custom_config.h
@@ -9,18 +9,18 @@
 KC_Q,              KC_W,              KC_E,              KC_R,              KC_T,              KC_Y,              KC_U,              KC_I,              KC_O,              KC_P,              \
 LGUI_T(KC_A),      LALT_T(KC_S),      LCTL_T(KC_D),      LSFT_T(KC_F),      KC_G,              KC_H,              LSFT_T(KC_J),      LCTL_T(KC_K),      LALT_T(KC_L),      LGUI_T(KC_QUOT),   \
 LT(U_BUTTON,KC_Z), ALGR_T(KC_X),      KC_C,              KC_V,              KC_B,              KC_N,              KC_M,              KC_COMM,           ALGR_T(KC_DOT),    LT(U_BUTTON,KC_SLSH),\
-U_NP,              U_NP,              LT(U_FUN,KC_DEL),  LT(U_NUM,KC_BSPC), OSM(MOD_LSFT),     LT(U_SYM,KC_SPC),  LT(U_NAV,KC_SPC),  LT(U_MEDIA,KC_ESC),U_NP,              U_NP
+U_NP,              U_NP,              LT(U_FUN,KC_DEL),  LT(U_NUM,KC_TAB),  OSM(MOD_LSFT),     LT(U_SYM,KC_SPC),  LT(U_NAV,KC_SPC),  LT(U_MEDIA,KC_ESC),U_NP,              U_NP
 
 
 #define MIRYOKU_LAYER_EXTRA \
 KC_Q,              KC_W,              KC_E,              KC_R,              KC_T,              KC_Y,              KC_U,              KC_I,              KC_O,              KC_P,              \
 KC_A,              KC_S,              KC_D,              KC_F,              KC_G,              KC_H,              KC_J,              KC_K,              KC_L,              KC_QUOT,           \
 KC_Z,              KC_X,              KC_C,              KC_V,              KC_B,              KC_N,              KC_M,              KC_COMM,           KC_DOT,            KC_SLSH,           \
-U_NP,              U_NP,              LT(U_FUN,KC_DEL),  LT(U_NUM,KC_BSPC), OSM(MOD_LSFT),     LT(U_SYM,KC_SPC),  LT(U_NAV,KC_SPC),  LT(U_MEDIA,KC_ESC),U_NP,              U_NP
+U_NP,              U_NP,              LT(U_FUN,KC_DEL),  LT(U_NUM,KC_TAB),  OSM(MOD_LSFT),     LT(U_SYM,KC_SPC),  LT(U_NAV,KC_SPC),  LT(U_MEDIA,KC_ESC),U_NP,              U_NP
 
 
 #define MIRYOKU_LAYER_SYM \
-KC_LCBR,           KC_AMPR,           KC_ASTR,           KC_LPRN,           KC_RCBR,           U_NA,              DF(U_BASE),        DF(U_EXTRA),       DF(U_TAP),         RESET,             \
-KC_COLN,           KC_DLR,            KC_PERC,           KC_CIRC,           KC_PLUS,           MO(U_MOUSE),       KC_LSFT,           KC_LCTL,           KC_LALT,           KC_LGUI,           \
+KC_LCBR,           KC_AMPR,           KC_ASTR,           KC_LPRN,           KC_RCBR,           U_NA,              DF(U_BASE),        DF(U_EXTRA),       U_NA,              RESET,             \
+KC_COLN,           KC_DLR,            KC_PERC,           KC_CIRC,           KC_PLUS,           MO(U_MOUSE),       KC_BSPC,           KC_BSPC,           KC_BSPC,           KC_BSPC,           \
 KC_TILD,           KC_EXLM,           KC_AT,             KC_HASH,           KC_PIPE,           KC_UNDS,           KC_MINUS,          KC_LT,             KC_GT,             KC_QUESTION,              \
 U_NP,              U_NP,              KC_LPRN,           KC_RPRN,           KC_UNDS,           U_NA,              U_NA,              U_NA,              U_NP,              U_NP
