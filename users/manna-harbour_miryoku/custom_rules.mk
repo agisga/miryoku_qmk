20c1abd4007f645163e11b89db47306b3a3bd43d
agisga <11449372+agisga@users.noreply.github.com>
2022-07-15 21:38:28 -0400

    removes custom shift keys behavior (i.e., key overrides)

--- users/manna-harbour_miryoku/custom_rules.mk
+++ users/manna-harbour_miryoku/custom_rules.mk
@@ -1,4 +1,3 @@
 # Copyright 2019 Manna Harbour
 # https://github.com/manna-harbour/miryoku
-KEY_OVERRIDE_ENABLE = yes  # shift+backspace is delete, etc.
 DYNAMIC_MACRO_ENABLE = yes  # see https://beta.docs.qmk.fm/using-qmk/advanced-keycodes/feature_dynamic_macros
