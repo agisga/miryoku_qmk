7097e71dac8c2f8af41365aa866d1e596c2fbc01
agisga <11449372+agisga@users.noreply.github.com>
2022-07-15 21:46:30 -0400

    timeout for one shot shift added

--- users/manna-harbour_miryoku/custom_config.h
+++ users/manna-harbour_miryoku/custom_config.h
@@ -5,6 +5,8 @@
 
 #pragma once
 
+#define ONESHOT_TIMEOUT 1000  /* Time (in ms) before the one shot key is released */
+
 #define MIRYOKU_LAYER_BASE \
 KC_Q,              KC_W,              KC_E,              KC_R,              KC_T,              KC_Y,              KC_U,              KC_I,              KC_O,              KC_P,              \
 LGUI_T(KC_A),      LALT_T(KC_S),      LCTL_T(KC_D),      LSFT_T(KC_F),      KC_G,              KC_H,              LSFT_T(KC_J),      LCTL_T(KC_K),      LALT_T(KC_L),      LGUI_T(KC_QUOT),   \
