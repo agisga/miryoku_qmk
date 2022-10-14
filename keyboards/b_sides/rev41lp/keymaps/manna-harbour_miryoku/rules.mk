ebf8e54a9da3ce895726ff2b058a6e128af1acd3
agisga <11449372+agisga@users.noreply.github.com>
2022-10-14 01:34:19 -0400

    initial setup for rev41lp

---
+++ keyboards/b_sides/rev41lp/keymaps/manna-harbour_miryoku/rules.mk
@@ -0,0 +1,10 @@
+# Copyright 2021 Manna Harbour
+# https://github.com/manna-harbour/miryoku
+
+# for saving firmware size (https://github.com/qmk/qmk_firmware/blob/master/docs/squeezing_avr.md):
+RGBLIGHT_ENABLE = no
+RGB_MATRIX_ENABLE = no
+BACKLIGHT_ENABLE = no
+SPACE_CADET_ENABLE = no
+GRAVE_ESC_ENABLE = no
+MAGIC_ENABLE = no
