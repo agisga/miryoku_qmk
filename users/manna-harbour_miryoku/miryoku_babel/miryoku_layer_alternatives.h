ab41bd739c2ec8b2988ddea47252d87c5d00f3fd
agisga <11449372+agisga@users.noreply.github.com>
2023-11-15 12:01:20 -0500

    tabedit macro

--- users/manna-harbour_miryoku/miryoku_babel/miryoku_layer_alternatives.h
+++ users/manna-harbour_miryoku/miryoku_babel/miryoku_layer_alternatives.h
@@ -383,7 +383,7 @@ U_NP,              U_NP,              KC_APP,            KC_SPC,            KC_T
 
 
 #define MIRYOKU_ALTERNATIVES_BUTTON \
-KC_COLONQ,         KC_COLONW,         KC_COLONWQ,        KC_COLONQEX,       KC_COLN,           U_NU,              U_NU,              RALT(KC_Y),        KC_LPRN,           KC_RPRN,           \
+KC_COLONQ,         KC_COLONW,         KC_COLONWQ,        KC_COLONQEX,       KC_TABEDIT,        U_NU,              U_NU,              RALT(KC_Y),        KC_LPRN,           KC_RPRN,           \
 RALT(KC_Q),        KC_AUTOCLOS_CURLYBRA,RALT(KC_S),        KC_AUTOCLOS_SQBRA, KC_AUTOCLOS_ANGLEBRA,KC_TODO,           KC_AUTOCLOS_QUOTE, KC_AUTOCLOS_PAREN, KC_FIFTEENSTARS,   RALT(KC_P),        \
 U_NU,              U_NU,              KC_VOLD,           KC_VOLU,           KC_MUTE,           KC_LT,             KC_LEFTARR,        KC_ELLIPSIS,       KC_RIGHTARR,       KC_GT,             \
 U_NP,              U_NP,              QK_DYNAMIC_MACRO_RECORD_STOP,QK_DYNAMIC_MACRO_PLAY_2,QK_DYNAMIC_MACRO_RECORD_START_2,QK_DYNAMIC_MACRO_RECORD_START_1,QK_DYNAMIC_MACRO_PLAY_1,QK_DYNAMIC_MACRO_RECORD_STOP,U_NP,              U_NP
